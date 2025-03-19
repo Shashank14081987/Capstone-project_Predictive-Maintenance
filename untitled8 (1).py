229be78edc69f7d886b8f3131617f63e486842b7
Shashank14081987 <jupower050988@gmail.com>
2025-03-19 07:07:43 +0530

    Update untitled8 (1).py

--- untitled8 (1).py
+++ untitled8 (1).py
@@ -66,9 +66,6 @@ describe_data(df)
 for column in df.columns:
   if df[column].dtype in ['int64', 'float64']:  # Check if the column is numeric
 
-# Encode categorical variable 'Type'
-df["Type"] = label_encoder.fit_transform(df["Type"])
-
 # Define features and target
 X = df.drop(columns=["Target"])
 y = df["Target"]
