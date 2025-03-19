be28dcc35b52e5972130ac1f524954103052727d
Shashank14081987 <jupower050988@gmail.com>
2025-03-19 07:00:08 +0530

    Update untitled8 (1).py

--- untitled8 (1).py
+++ untitled8 (1).py
@@ -9,7 +9,6 @@ Original file is located at
 
 import pandas as pd
 import numpy as np
-from sklearn.model_selection import train_test_split, cross_val_score
 from sklearn.preprocessing import StandardScaler, LabelEncoder
 from sklearn.linear_model import LinearRegression, Ridge, Lasso, ElasticNet
 from sklearn.tree import DecisionTreeRegressor
